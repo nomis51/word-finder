c62b8e5f8bc207c9191f371620a2369c33d79ed0
nomis51 <nomis.guit.51@gmail.com>
2022-08-06 16:31:26 -0300

    csharp impl

--- app-dotnet/WordFinder/Program.cs
+++ app-dotnet/WordFinder/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+
+namespace WordFinder;
+
+public static class Program {
+    public static void Main()
+    {
+      new Cli().Execute();
+    }
+}
